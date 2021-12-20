4fcd8ea6d05ec798f9ce5522f99474b2c6826e61
Nguyễn Quang Sỹ <83626285+nguyenquangsy@users.noreply.github.com>
2021-12-20 09:32:41 +0700

    first

---
+++ DOAN/src/Client/Client.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author xyzsy
+ */
+public class Client {
+    public final static String SERVER_IP = "127.0.0.1";
+    public final static int SERVER_PORT = 1433; // Cổng mặc định của Echo Server
+    public final static byte[] BUFFER = new byte[4096]; // Vùng đệm chứa dữ liệu cho gói tin nhận
+ 
+    public static void main(String[] args) {
+        DatagramSocket ds = null;
+        String str = "";
+        int key;
+        try {
+            ds = new DatagramSocket(); // Tạo DatagramSocket
+            System.out.println("Client started ");
+ 
+            InetAddress server = InetAddress.getByName(SERVER_IP);
+            while (true) {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Nhap van ban: ");
+                str = sc.nextLine();
+                System.out.print("Nhap khoa: ");
+                key = sc.nextInt();
+                
+                String text = encode(str, key);
+                byte[] data = text.getBytes(); // Đổi chuỗi ra mảng bytes
+ 
+                // Tạo gói tin gởi
+                DatagramPacket dp = new DatagramPacket(data, data.length, server, SERVER_PORT);
+                ds.send(dp); // Send gói tin sang Echo Server
+ 
+                // Gói tin nhận
+                DatagramPacket incoming = new DatagramPacket(BUFFER, BUFFER.length);
+                ds.receive(incoming); // Chờ nhận dữ liệu từ EchoServer gởi về
+ 
+                // Đổi dữ liệu nhận được dạng mảng bytes ra chuỗi và in ra màn hình
+                System.out.println("So luong xuat hien cua cac chu cai: " + new String(incoming.getData(), 0, incoming.getLength()));
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+        }
+    }
+    
+    private static String encode(String str, int key){
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))) {
+                char ch = (char) (((int) str.charAt(i) +
+                        key - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) str.charAt(i) +
+                        key - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        
+        return result.toString();
+    }
+}
---
+++ DOAN/src/Server/Server.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author xyzsy
+ */
+public class Server {
+    public final static int SERVER_PORT = 1433; // Cổng mặc định của Echo Server
+    public final static byte[] BUFFER = new byte[4096]; // Vùng đệm chứa dữ liệu cho gói tin nhận
+ 
+    public static void main(String[] args) {
+        DatagramSocket ds = null;
+        int key = 2;
+        try {
+            System.out.println("Binding to port " + SERVER_PORT + ", please wait  ...");
+            ds = new DatagramSocket(SERVER_PORT); // Tạo Socket với cổng là 7
+            System.out.println("Server started ");
+            System.out.println("Waiting for messages from Client ... ");
+ 
+            while (true) { // Tạo gói tin nhận
+                DatagramPacket incoming = new DatagramPacket(BUFFER, BUFFER.length);
+                ds.receive(incoming); // Chờ nhận gói tin gởi đến
+ 
+                // Lấy dữ liệu khỏi gói tin nhận
+                String message = new String(incoming.getData(), 0, incoming.getLength());
+                System.out.println("Van ban nhap duoc: " + message);
+                
+                //Giải mã
+                String text = decode(message, 26-key);
+                System.out.println("bản rõ: " + text);
+                
+                //Đếm số lượng 
+                List<String> chucai = new ArrayList<String>();
+                
+                for(int i = 0; i < text.length(); i ++){
+                    if(check(chucai, String.valueOf(text.charAt(i)))){
+                        chucai.add(String.valueOf(text.charAt(i)));
+                    }
+                }
+                
+                String send = "";
+                int dem = 0;
+                for(int i = 0; i < chucai.size(); i ++)
+                {
+                    for(int j = 0; j < text.length(); j ++){
+                        String kytu = String.valueOf(text.charAt(j));
+                        if(chucai.get(i).equals(kytu)){
+                            dem ++;
+                        }
+                    }
+                    if(i == chucai.size()-1){
+                        send += chucai.get(i)+": "+dem;
+                    }else{
+                        send += chucai.get(i)+": "+dem+" - ";
+                    }
+                    dem = 0;
+                }
+                
+ 
+                // Tạo gói tin gởi chứa dữ liệu vừa nhận được
+                DatagramPacket outsending = new DatagramPacket(send.getBytes(), incoming.getLength(),
+                        incoming.getAddress(), incoming.getPort());
+                ds.send(outsending);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+        }
+    }
+    
+    private static String decode(String str, int key){
+        StringBuffer result = new StringBuffer();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isUpperCase(str.charAt(i))) {
+                char ch = (char) (((int) str.charAt(i) +
+                        key - 65) % 26 + 65);
+                result.append(ch);
+            } else {
+                char ch = (char) (((int) str.charAt(i) +
+                        key - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+    
+    private static boolean check(List<String> chucai, String str){
+        for(int i = 0; i < chucai.size(); i ++){
+            if(chucai.get(i).equals(str)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
